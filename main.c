88bb1077dbaeb32d6af088274a95cd6ae54c12de
Johann Verlohner <verlohnerjohann@gmail.com>
2025-01-30 16:47:04 -0800

    fix the whle compilation thing

--- main.c
+++ main.c
@@ -1,28 +1,13 @@
-#include <stdbool.h>
+#include <stdio.h>
 #include <SDL2/SDL.h>
 
-const int WIDTH = 800, HEIGHT = 600;
+#define WIDTH 900
+#define HEIGHT 600
 
-int main( int argc, char *argv[] )
+int main(int argc, char *argv[])
 {
-    SDL_Init( SDL_INIT_EVERYTHING );
-
-    SDL_Window *window = SDL_CreateWindow( "Hello SDL WORLD", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI );
-
-
-    SDL_Event windowEvent;
-
-    while ( true )
-    {
-        if ( SDL_PollEvent( &windowEvent ) )
-        {
-            if ( SDL_QUIT == windowEvent.type )
-            { break; }
-        }
-    }
-
-    SDL_DestroyWindow( window );
-    SDL_Quit( );
-
-    return EXIT_SUCCESS;
+        SDL_Init(SDL_INIT_VIDEO);
+        SDL_Window *window = SDL_CreateWindow("Window Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
+        SDL_Delay(5000);
+        return 0;
 }
